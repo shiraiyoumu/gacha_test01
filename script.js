411dbf25f7047cee154782fe38700574cd7f94c1
shiraiyoumu <shun.oshita@gmail.com>
2025-04-29 17:44:49 +0900

    ガチャ表示内容修正

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const menu = [
-    { name: "シュラスコMIXイチボプレート", 1600: ,shop:"Bear's kitchen" },
-    { name: "国産牛100%!!武骨BEEFハンバーグ BENTO", price:1400 ,shop:"BULL`S" },
-    { name: "チャンピオン弁当", price:1700 ,shop:"北海道ジンギスカン Lamb`z" },
+    { menu: "シュラスコMIXイチボプレート",price: 1600 ,shop:"Bear's kitchen" },
+    { menu: "国産牛100%!!武骨BEEFハンバーグ BENTO", price:1400 ,shop:"BULL`S" },
+    { menu: "チャンピオン弁当", price:1700 ,shop:"北海道ジンギスカン Lamb`z" },
     // { name: "温玉とり天丼", price:950 ,shop:"いちや" },
     // // { name: "", price: ,shop:"" },
     // { name: "", price: ,shop:"" },
@@ -45,7 +45,11 @@ button.addEventListener("click", function() {
     } else {
         let html = "<h2>ガチャ結果🎯</h2><ul>";
         selectedItems.forEach(item => {
-            html += `<li>${item.name}（${item.shop}）：${item.price}円</li>`;
+            html += `<li>
+                メニュー名：${item.manu}<br>
+                店舗名：${item.shop}<br>
+                金額：${item.price}円
+            </li><br>`;
         });
         html += `</ul><p>合計金額：${total}円</p>`;
         resultDiv.innerHTML = html;
